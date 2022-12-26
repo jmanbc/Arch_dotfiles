19e5ba3f1df5838049093713132ab55342f477f0
jmboles <jmanbc@protonmail.com>
2022-12-26 16:16:21 -0500

    Added Doom with other updates

---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ivy-rtags helm-rtags ccls opencl-mode modern-cpp-font-lock disaster demangle-mode cuda-mode company-glsl cmake-mode flycheck-rtags company-rtags rtags eglot flycheck-irony company-irony-c-headers company-irony irony-eldoc irony image-dired+ beacon)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
