5fc8c163913fbb8b60f5ea4960eb4b5d110dc6e7
jmboles <jmanbc@protonmail.com>
2023-03-18 15:09:21 -0400

    Updates

--- .config/rofi/powermenu/type-1/powermenu.sh
+++ .config/rofi/powermenu/type-1/powermenu.sh
@@ -98,7 +98,7 @@ case ${chosen} in
 		if [[ -x '/usr/bin/betterlockscreen' ]]; then
 			betterlockscreen -l
 		elif [[ -x '/usr/bin/i3lock' ]]; then
-			lock
+			lock & dunstctl set-paused true
 		fi
         ;;
     $suspend)
